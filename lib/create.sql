18124d4e6fb7ec2e1952b04a332268007ae491f6
Cody Yantis <cyantis@gmail.com>
2019-04-21 20:48:53 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,24 @@
-CREATE TABLE project (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date TEXT,
-  end_date TEXT
-);
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal NUMERIC,
+start_date TEXT,
+end_date TEXT
+)
+;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
-);
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+)
+;
 
-CREATE TABLE pledge (
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount NUMERIC,
+user_id INTEGER,
+project_id INTEGER
+)
+;
