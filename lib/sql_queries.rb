6406321ecc0750c141a0dda2dfad1279c2bcf24e
Cody Yantis <cyantis@gmail.com>
2019-04-21 21:19:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, pledges.amount - projects.funding_goal
 FROM projects
 JOIN pledges ON projects.id = pledges.project_id
-GROUP BY projects.title
+GROUP BY projects.id
 HAVING projects.funding_goal <= pledges.amount;"
 end
 
