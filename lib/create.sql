96155cb6db0430d22fe4c3ab666f543311cf4371
Cody Yantis <cyantis@gmail.com>
2019-04-21 20:47:18 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,11 @@
-CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY,
